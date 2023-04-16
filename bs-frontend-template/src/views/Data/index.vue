ca5ba5c849289b972ad66588b023dd9be0ade81c
haoc.jin <haoc.jin@sunyard.com>
2023-04-16 22:51:43 +0800

    分页功能完善

--- bs-frontend-template/src/views/Data/index.vue
+++ bs-frontend-template/src/views/Data/index.vue
@@ -1,7 +1,7 @@
 <template>
     <div style='padding: 30px'>
-        <el-row type='flex' justify='center'>
-            <el-form :model='commentForm' :inline='true'>
+        <el-row justify='center' type='flex'>
+            <el-form :inline='true' :model='commentForm'>
                 <el-form-item label='标题'>
                     <el-input
                         v-model='commentForm._title__icontains'
@@ -36,11 +36,11 @@
             </el-form>
         </el-row>
         <el-table :data='commentList' style='width: 100%; margin: 20px 10px'>
-            <el-table-column label='封面' prop='img' align='center' width='180'>
+            <el-table-column align='center' label='封面' prop='img' width='180'>
                 <template #default='{ row }'>
                     <el-link
-                        type='primary'
                         :underline='false'
+                        type='primary'
                         @click='viewNewsDetail(row.id)'
                     >
                         <el-image
@@ -54,11 +54,11 @@
                     </el-link>
                 </template>
             </el-table-column>
-            <el-table-column label='标题' prop='title' sortable align='center'>
+            <el-table-column align='center' label='标题' prop='title' sortable>
                 <template #default='{ row }'>
                     <el-link
-                        type='primary'
                         :underline='false'
+                        type='primary'
                         @click='viewNewsDetail(row.id)'
                     >
                         {{ row.title }}
@@ -67,21 +67,21 @@
             </el-table-column>
             <el-table-column label='简介' prop='intro' sortable>
                 <template #default='{ row }'>
-                    <el-link type='info' :underline='false' @click='showDl(row.intro)'>
+                    <el-link :underline='false' type='info' @click='showDl(row.intro)'>
                         {{ ellipsis(row.intro) }}
                     </el-link>
                 </template>
             </el-table-column>
-            <el-table-column label='标签' prop='keywords' width='180' sortable>
+            <el-table-column label='标签' prop='keywords' sortable width='180'>
                 <template #default='{ row }'>
                     <template v-if='row.keywords'>
                         <el-tag
-                            v-for='t in row.keywords.split(&apos;,&apos;)'
+                            v-for='t in row.keywords.split(",")'
                             :key='t'
-                            type='info'
-                            style='margin: 0 5px 5px 0; cursor: pointer'
                             effect='plain'
                             size='mini'
+                            style='margin: 0 5px 5px 0; cursor: pointer'
+                            type='info'
                             @click='
                                 commentForm._keywords__icontains = t;
                                 search();
@@ -94,38 +94,47 @@
             <el-table-column
                 label='来源媒体'
                 prop='media_name'
-                width='150'
                 sortable
+                width='150'
             />
-            <el-table-column label='发布日期' prop='intime' width='150' sortable />
-            <el-table-column label='点击率' prop='view_count' width='150' sortable />
-            <el-table-column label='操作' align='center' width='180' sortable>
+            <el-table-column label='发布日期' prop='intime' sortable width='150' />
+            <el-table-column label='点击率' prop='view_count' sortable width='150' />
+            <el-table-column align='center' label='操作' sortable width='180'>
                 <template #default='scope'>
                     <el-button
-                        type='primary'
                         size='mini'
+                        type='primary'
                         @click='viewCommentDetail(scope.row.text, scope.row.subject)'
                     >内容分析
                     </el-button>
                 </template>
             </el-table-column>
         </el-table>
-        <el-row type='flex' justify='center' style='margin-top: 30px'>
+        <el-row justify='center' style='margin-top: 30px' type='flex'>
+            <!--            <el-pagination-->
+            <!--                :current-page='commentForm.page'-->
+            <!--                :page-size='commentForm.pagesize'-->
+            <!--                layout='prev, pager, next, jumper, total'-->
+            <!--                :total='commentForm.total'-->
+            <!--                background-->
+            <!--                @current-change='handleCurrentChange'-->
+            <!--            />-->
             <el-pagination
                 :current-page='commentForm.page'
                 :page-size='commentForm.pagesize'
-                layout='prev, pager, next, jumper, total'
+                :page-sizes='pagesize'
                 :total='commentForm.total'
-                background
+                layout='sizes, prev, pager, next, jumper, total'
+                @size-change='handleSizeChange'
                 @current-change='handleCurrentChange'
             />
         </el-row>
         <el-dialog
             v-model='centerDialogVisible'
+            center
+            destroy-on-close
             title='简介'
             width='70%'
-            destroy-on-close
-            center
         >
             <el-card
                 shadow='always'
@@ -144,13 +153,14 @@ export default {
     data() {
         return {
             text: '',
+            pagesize: [10, 20, 50, 100],
             commentList: [],
             commentForm: {
                 _txts__icontains: null,
                 _positive: null,
                 total: 0,
                 page: 1,
-                pagesize: 50
+                pagesize: 20
             },
             centerDialogVisible: false
         }
@@ -168,7 +178,7 @@ export default {
                 const reg = new RegExp(this.commentForm._txts__icontains, 'ig')
                 return this.text.replace(
                     reg,
-                    `<span style="color:red;font-weight:bold;">${txt}</span>`
+                    `<span style='color:red;font-weight:bold;'>${txt}</span>`
                 )
             }
             return this.text
@@ -191,22 +201,28 @@ export default {
             this.commentForm.page = page
             this.getlist()
         },
+        handleSizeChange(val) {
+            this.commentForm.pagesize = val
+            this.getlist()
+        },
         search() {
             this.commentForm.page = 1
             this.getlist()
         },
         getlist() {
-            request.post('/news/', this.commentForm).then((res) => {
-                this.commentList = res.data.result
-                this.commentForm.total = res.data.total
-            })
+            request.post('/news/', this.commentForm)
+                .then((res) => {
+                    this.commentList = res.data.result
+                    this.commentForm.total = res.data.total
+                    console.log(res)
+                })
         }
     }
 }
 </script>
 
 
-<style lang="postcss" scoped>
+<style lang='postcss' scoped>
 .image-slot {
     display: flex;
     justify-content: center;
