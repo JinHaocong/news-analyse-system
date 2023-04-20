c484fbd02b9fcd0e7a23bb0146f2fd2a703debfb
haoc.jin <haoc.jin@sunyard.com>
2023-04-20 21:04:49 +0800

    2023/04/20

--- mynlp/classification/analysis.py
+++ mynlp/classification/analysis.py
@@ -26,9 +26,9 @@ from tensorflow.python.keras.models import load_model
 
 class SentimentAnalysis:
     def __init__(self, model_path, tokenizer_path, positive_path, negative_path, ):
-        self.num_words = 40000  # 初始化 Tokenizer 对象时指定的参数，用于控制词汇表的大小，仅保留出现频率最高的 num_words 个词。
+        self.num_words = 60000  # 初始化 Tokenizer 对象时指定的参数，用于控制词汇表的大小，仅保留出现频率最高的 num_words 个词。
         self.embedding_dim = 64  # 词嵌入的维度
-        self.max_length = 4000  # 输入序列的最大长度
+        self.max_length = 400  # 输入序列的最大长度
         self.filters = 64  # 卷积层的滤波器个数
         self.kernel_size = 10  # 卷积核的大小
         self.pool_size = 10  # 池化层的大小
