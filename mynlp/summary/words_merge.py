399a10ac745fb033953238d6bd19cbbd321fd9e5
JinHaocong <jinhaocong@outlook.com>
2023-04-27 17:05:56 +0800

    2023/04/27

--- mynlp/summary/words_merge.py
+++ mynlp/summary/words_merge.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 import re
 
+from mynlp.utils.trie import Trie
+
 """
 SimpleMerge 的类，实现了一种简单的字符串合并算法。
 其构造函数 init() 接收两个参数，分别是待处理的字符串 doc 和包含需要合并的词和相应权重的元组列表 words。
@@ -20,43 +22,58 @@ class SimpleMerge(object):
     def __init__(self, doc, words):
         self.doc = doc
         self.words = words
+        self.trie = Trie()  # 初始化 Trie 类
+
+        # 将关键字插入 Trie 中
+        for word in words:
+            self.trie.insert(word[0], word[1])
 
     def merge(self):
         trans = {}
-        for w in self.words:
-            trans[w[0]] = ('', w[1])
-        for w1 in self.words:
+
+        # 遍历 Trie，查找关键字
+        for word in self.words:
+            value = self.trie.find(word[0])
+            if value is not None:
+                trans[word[0]] = ('', value[1])
+
+        for word1 in self.words:
             cw = 0
-            lw = len(w1[0])
-            pattern = re.compile(w1[0])
+            lw = len(word1[0])
+            pattern = re.compile(word1[0])
             for match in pattern.findall(self.doc):
-                cw += w1[1]
-            for w2 in self.words:
+                cw += word1[1]
+
+            for word2 in self.words:
                 cnt = 0
-                l2 = len(w1[0]) + len(w2[0])
-                pattern = re.compile(w1[0] + w2[0])
+                l2 = len(word1[0]) + len(word2[0])
+                pattern = re.compile(word1[0] + word2[0])
                 for match in pattern.findall(self.doc):
-                    cnt += w2[1]
+                    cnt += word2[1]
                 if cw < cnt * 2:
-                    trans[w1[0]] = (w2[0], w1[1] + w2[1])
-                    break
+                    value = self.trie.find(word2[0])
+                    if value is not None:
+                        trans[word1[0]] = (word2[0], word1[1] + value[1])
+                        break
+
         ret = []
-        for w in self.words:
-            if w[0] not in trans:
+        for word in self.words:
+            if word[0] not in trans:
                 continue
             s = ''
-            weight = trans[w[0]][1]
-            now = trans[w[0]][0]
+            weight = trans[word[0]][1]
+            now = trans[word[0]][0]
             while now:
                 s += now
-                weight += w[1]
+                weight += word[1]
                 if now not in trans:
                     break
                 tmp = trans[now]
                 del trans[now]
                 now = tmp[0]
-            trans[w[0]] = (s, weight)
-        for w in self.words:
-            if w[0] in trans:
-                ret.append((w[0] + trans[w[0]][0], trans[w[0]][1]))
+            trans[word[0]] = (s, weight)
+
+        for word in self.words:
+            if word[0] in trans:
+                ret.append((word[0] + trans[word[0]][0], trans[word[0]][1]))
         return ret
