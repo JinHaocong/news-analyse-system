2cff82a428d3a6e130ce9d5309badf2deda9573e
JinHaocong <jinhaocong@outlook.com>
2023-05-04 17:27:39 +0800

    2023/05/04

--- mynlp/seg/seg.py
+++ mynlp/seg/seg.py
@@ -41,7 +41,7 @@ class Seg(object):
             line = i.strip()
             if not line:
                 continue
-            tmp = map(lambda x: x.split('/'), line.split())
+            tmp = [x.split('/') for x in line.split()]
             data.append(tmp)
         fr.close()
         self.segger.train(data)
@@ -49,21 +49,35 @@ class Seg(object):
     def seg(self, sentence):
         ret = self.segger.tag(sentence)
         tmp = ''
+        result = []
         for i in ret:
             if i[1] == 'e':
-                yield tmp + i[0]
+                result.append(tmp + i[0])
                 tmp = ''
             elif i[1] == 'b' or i[1] == 's':
                 if tmp:
-                    yield tmp
+                    result.append(tmp)
                 tmp = i[0]
             else:
                 tmp += i[0]
         if tmp:
-            yield tmp
+            result.append(tmp)
+        return result
+
+
+def seg_test(text):
+    my_seg = Seg()
+    my_seg.load('seg.marshal')
+    print(' '.join(my_seg.seg(text)))
+
+
+def seg_train(text):
+    my_seg = Seg()
+    my_seg.train('data.txt')
+    my_seg.save('seg.marshal')
+    print(' '.join(my_seg.seg(text)))
 
 
 if __name__ == '__main__':
-    seg = Seg()
-    seg.train('data.txt')
-    print(' '.join(seg.seg('主要是用来放置一些简单快速的中文分词和词性标注的程序')))
+    # seg_train('基于三阶隐马尔可夫模型的中文文本分词算法')
+    seg_test('主要是用来放置一些简单快速的中文分词的程序')
